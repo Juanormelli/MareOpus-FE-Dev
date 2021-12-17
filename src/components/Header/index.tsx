a0f0423a3049acded547cf20ed711b2ebacb21be
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-12-17 11:56:39 -0300

    Estrutura Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+
+import logo from "/img/Logo.svg"
+
+
+export default function Header(){
+    return(
+        <>
+    
+            <div>
+                <img src="/img/Logo.svg"alt="teste" />
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Planos</li>
+
+                    </ul>
+                    <button>
+                        <img src="" alt="teste" />
+                        Entrar
+                    </button>
+                    <button type="button">
+                        Registrar
+                    </button>
+                    <ul>
+                        <button><img src="teste" alt="teste" /></button>
+                        <button><img src="teste" alt="teste" /></button>
+                        <button><img src="teste" alt="teste" /></button>
+                    </ul>
+                </nav>
+            </div>
+            
+
+        </>
+        
+    )
+}
