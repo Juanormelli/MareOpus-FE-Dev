065f2e2a142ccb610c52d4ee0f25e919d9a5b931
Guilherme Sanches Simões <guilhermebr18@hotmail.com>
2022-01-04 12:31:48 -0300

    Ajustes 04012022

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from './styles.module.scss';
 import { ActiveLink } from '../ActiveLink';
-import { useTranslation } from 'next-i18next';
+//import { useTranslation } from 'next-i18next';
 import { signOut, useSession } from 'next-auth/client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+//import Link from 'next/link';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { AiOutlineClose } from 'react-icons/ai';
+//import { Link, useHistory } from 'react-router-dom';
 
 interface HeaderProps {
   onOpenLoginModal: () => void;
@@ -11,58 +15,100 @@ interface HeaderProps {
 }
 
 export default function Header(props: HeaderProps) {
+  // Abrir Meu
+  //const history = useHistory();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
   const [session] = useSession();
 
   useEffect(() => {}, [session]);
+
   //const { t, i18n } = useTranslation();
   if (session) {
     return (
       <>
-        <header className={styles.headerContainer}>
-          <div className={styles.headerContent}>
-            <img src="/img/Logo.svg" alt="Logo" className={styles.imgLogin} />
-            <div className={styles.toggle}></div>
-            <ul className={styles.navigation}>
-              <li>
-                <ActiveLink activateClassname={styles.active} href="/">
-                  <a>Home</a>
-                </ActiveLink>
-              </li>
-              <li>
-                <ActiveLink activateClassname={styles.active} href="/planos">
-                  <a>Planos</a>
-                </ActiveLink>
-              </li>
-              <li>
-                <a
-                  className={styles.navbar_link}
-                  onClick={props.onOpenLoginModal}
-                >
-                  <img src="/icon/entar.svg" alt="entrar" />
-                  ENTRAR
-                </a>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  className={styles.signinButton}
-                  onClick={() => signOut()}
-                >
-                  <a>REGISTRAR</a>
-                </button>
-              </li>
-              <li>
-                <a className={styles.lengButton}>
-                  <img src="/icon/PORTUGUES.svg" alt="Portugues" />
-                </a>
-                <a className={styles.lengButton}>
-                  <img src="/icon/INGLES.svg" alt="Inglês" />
-                </a>
-                <a className={styles.lengButton}>
-                  <img src="/icon/ESPANHOL.svg" alt="Espanhol" />
-                </a>
-              </li>
-            </ul>
+        <header className={styles.header}>
+          <div className={styles.header__content}>
+            <ActiveLink activateClassname={styles.imgLogin} href="/">
+              <img src="/img/Logo.svg" alt="Logo" className={styles.imgLogin} />
+            </ActiveLink>
+
+            <nav
+              className={`${styles.header__content__nav} ${
+                menuOpen && size.width < 768 ? styles.isMenu : ''
+              }`}
+            >
+              <ul className={styles.navigation}>
+                <li>
+                  <ActiveLink activateClassname={styles.active} href="/">
+                    <a>Home</a>
+                  </ActiveLink>
+                </li>
+                <li>
+                  <ActiveLink activateClassname={styles.active} href="/planos">
+                    <a>Planos</a>
+                  </ActiveLink>
+                </li>
+                <li>
+                  <a
+                    className={styles.navbar_link}
+                    onClick={props.onOpenLoginModal}
+                  >
+                    <img src="/icon/entar.svg" alt="entrar" />
+                    ENTRAR
+                  </a>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className={styles.signinButton}
+                    onClick={() => signOut()}
+                  >
+                    <a>REGISTRAR</a>
+                  </button>
+                </li>
+              </ul>
+              <a className={styles.lengButton}>
+                <img src="/icon/PORTUGUES.svg" alt="Portugues" />
+              </a>
+              <a className={styles.lengButton}>
+                <img src="/icon/INGLES.svg" alt="Inglês" />
+              </a>
+              <a className={styles.lengButton}>
+                <img src="/icon/ESPANHOL.svg" alt="Espanhol" />
+              </a>
+            </nav>
+            <div className={styles.__content__toggle}>
+              {!menuOpen ? (
+                <img src="/icon/menu.svg" alt="open" />
+              ) : (
+                <AiOutlineClose onClick={menuToggleHandler} />
+              )}
+            </div>
           </div>
         </header>
       </>
@@ -70,47 +116,58 @@ export default function Header(props: HeaderProps) {
   } else {
     return (
       <>
-        <header className={styles.headerContainer}>
-          <div className={styles.headerContent}>
+        <header className={styles.header}>
+          <div className={styles.header__content}>
             <img src="/img/Logo.svg" alt="Logo" className={styles.imgLogin} />
-            <div className={styles.toggle}></div>
-            <ul className={styles.navigation}>
-              <li>
-                <ActiveLink activateClassname={styles.active} href="/">
-                  <a>Home</a>
-                </ActiveLink>
-              </li>
-              <li>
-                <ActiveLink activateClassname={styles.active} href="/planos">
-                  <a>Planos</a>
-                </ActiveLink>
-              </li>
-              <li>
-                <a
-                  className={styles.navbar_link}
-                  onClick={props.onOpenLoginModal}
-                >
-                  <img src="/icon/entar.svg" alt="entrar" />
-                  ENTRAR
-                </a>
-              </li>
-              <li>
-                <button type="button" className={styles.signinButton}>
-                  <a>REGISTRAR</a>
-                </button>
-              </li>
-              <li>
-                <a className={styles.lengButton} onClick={props.onOpenSession}>
-                  <img src="/icon/PORTUGUES.svg" alt="Portugues" />
-                </a>
-                <a className={styles.lengButton}>
-                  <img src="/icon/INGLES.svg" alt="Inglês" />
-                </a>
-                <a className={styles.lengButton}>
-                  <img src="/icon/ESPANHOL.svg" alt="Espanhol" />
-                </a>
-              </li>
-            </ul>
+
+            <nav
+              className={`${styles.header__content__nav} ${
+                menuOpen && size.width < 768 ? styles.isMenu : ''
+              }`}
+            >
+              <ul className={styles.navigation}>
+                <li>
+                  <ActiveLink activateClassname={styles.active} href="/">
+                    <a>Home</a>
+                  </ActiveLink>
+                </li>
+                <li>
+                  <ActiveLink activateClassname={styles.active} href="/planos">
+                    <a>Planos</a>
+                  </ActiveLink>
+                </li>
+                <li>
+                  <a
+                    className={styles.navbar_link}
+                    onClick={props.onOpenLoginModal}
+                  >
+                    <img src="/icon/entar.svg" alt="entrar" />
+                    ENTRAR
+                  </a>
+                </li>
+                <li>
+                  <button type="button" className={styles.signinButton}>
+                    <a>REGISTRAR</a>
+                  </button>
+                </li>
+              </ul>
+              <a className={styles.lengButton} onClick={props.onOpenSession}>
+                <img src="/icon/PORTUGUES.svg" alt="Portugues" />
+              </a>
+              <a className={styles.lengButton}>
+                <img src="/icon/INGLES.svg" alt="Inglês" />
+              </a>
+              <a className={styles.lengButton}>
+                <img src="/icon/ESPANHOL.svg" alt="Espanhol" />
+              </a>
+            </nav>
+            <div className={styles.__content__toggle}>
+              {!menuOpen ? (
+                <img src="/icon/menu.svg" alt="open" />
+              ) : (
+                <AiOutlineClose onClick={menuToggleHandler} />
+              )}
+            </div>
           </div>
         </header>
       </>
